aac42bec64242267a4425934c6abe2179af22c59
Luan Assis <luan2lobo@gmail.com>
2022-06-11 20:31:42 -0300

    Cart adicionado

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,23 @@
+import Modal from '../UI/Modal';
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = <ul className={classes['cart-items']}>{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
+    (item) => <li>{item.name}</li>)}</ul>;
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.50</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['buttton--alt']}>Close</button>
+        <button className={classes.button}>Oder</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
